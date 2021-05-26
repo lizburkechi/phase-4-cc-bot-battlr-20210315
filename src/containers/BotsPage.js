b74f4504c2cd6ce18f0151cb69afdaf43d8e3a7e
lizburkechi <liz.burke.chi@gmail.com>
2021-05-26 16:02:35 -0500

    first deliverable

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import BotCollection from './BotCollection'
 
 class BotsPage extends Component {
-  //start here with your code for step one
+   state = {
+    bots: [],
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:6001/bots")
+    .then(res => res.json())
+    .then(bots => this.setState({ bots }))
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return (
+    <div>
+      <BotCollection bots={this.state.bots} />
+    </div>
+    );
   }
 }
 
